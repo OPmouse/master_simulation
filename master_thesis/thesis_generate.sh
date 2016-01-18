cc414b66ff7ed3676de169bcae3bdd28cace8382
王昊 <wang_h@awcc.uec.ac.jp>
2016-01-19 01:45:11 +0900

    update chap.2 and reference.

--- master_thesis/thesis_generate.sh
+++ master_thesis/thesis_generate.sh
@@ -2,5 +2,7 @@
 platex cover.tex;dvipdfmx cover.dvi
 platex main.tex;platex main.tex;dvipdfmx main.dvi
 
-pdftk cover.pdf main.pdf cat output master_thesis.pdf
-evince master_thesis.pdf
+#pdftk cover.pdf main.pdf cat output master_thesis.pdf
+
+#evince master_thesis.pdf
+open main.pdf &
